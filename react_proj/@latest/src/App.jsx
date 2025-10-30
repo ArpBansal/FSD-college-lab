37fd95efd8b0aa826e7489a9c330ca39c0b0ec07
Arpbansal <arpitbansal761943@gmail.com>
2025-10-30 15:38:15 +0530

    started react proj

---
+++ react_proj/@latest/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Car from './vehicle'
+function App() {
+  
+
+  return (
+    <>
+      <Car />
+      <h1>Component Behaviour</h1>
+      <h2>Testing</h2>
+      <Car></Car>
+    </>
+  )
+}
+
+export default App
