d69f4a9bf66022d4deed46d3d6d700801a6a2a0c
Arpbansal <arpitbansal761943@gmail.com>
2025-11-07 11:01:13 +0530

    made components

--- react_proj/@latest/src/App.jsx
+++ react_proj/@latest/src/App.jsx
@@ -1,15 +1,25 @@
-import Car from './vehicle'
-import './App.css'
-function App() {
+import "./App.css"
+import Card from "./card"
 
-  return (
-    <>
-     <Car color="Red" brand="jeep"/>
-     <h1>testing component behavior</h1>
-     <h2>Testing</h2>
-     <Car color="Black" brand="Ford"/>
-    </>
+function App() {
+  return(
+    <div className="card-container">
+      <Card
+      image="https://marketplace.canva.com/EAGUhHGuQOg/1/0/1003w/canva-orange-and-blue-anime-cartoon-illustrative-novel-story-book-cover-WZE2VIj5AVQ.jpg"
+      title="React Basics"
+      description="Learn about components,props and state"
+      />
+       <Card
+      image="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/old-books-cover-design-template-528851dfc1b6ed275212cd110a105122_screen.jpg?ts=1698687093"
+      title="Java Script"
+      description="Master the language behind react"
+      />
+      <Card
+      image="https://miblart.com/wp-content/uploads/2024/01/main-6-scaled.jpg"
+      title="CSS Tricks"
+      description="Master the language behind react"
+      />
+    </div>
   )
 }
-
-export default App
+export default App;
